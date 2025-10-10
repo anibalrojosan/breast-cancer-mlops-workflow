acf8cf371f738e20cb9b30164de2e123be178036
Anibal Rojo <anibal.rojo16@gmail.com>
2025-10-10 18:48:01 -0300

    added unit test test_map_diagnosis_to_numerical_success

---
+++ tests/unit/data_preprocessing/test_map_diagnosis_to_numerical.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+from src.model.data_preprocessing import map_diagnosis_to_numerical
+
+def test_map_diagnosis_to_numerical_success(sample_data_1):
+    '''Test that M and B are correctly mapped to 1 and 0'''
+    df = sample_data_1.copy()
+    result = map_diagnosis_to_numerical(df)
+
+    # Assert specific mappings
+    assert result.iloc[0]['diagnosis'] == 1
+    assert result.iloc[1]['diagnosis'] == 0
+    assert result.iloc[2]['diagnosis'] == 1
+
+    # Assert that only 0s and 1s are present in the diagnosis column
+    assert sorted(result['diagnosis'].unique().tolist()) == [0, 1]
+
+    # Assert that the other columns remain the same
+    assert result['feature_1'].equals(sample_data_1['feature_1'])
+    assert result['feature_2'].equals(sample_data_1['feature_2'])
+    assert result['feature_3'].equals(sample_data_1['feature_3'])
